f9e5e69937f289dfeece41a1d4ccc3369bd9ecad
Chandrakant <chndyadav@gmail.com>
2020-06-25 00:59:42 +0530

    updating files

---
+++ textutils/textutils/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import string
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def analyze(request):
+    djtext = request.GET.get("text", "default")
+    removepunc = request.GET.get("removepunc", "off")
+    if removepunc == "on":
+        puntuations = string.punctuation  # to get the punctuations
+        print(puntuations)
+        analyzed = ""
+        for char in djtext:
+            if char not in puntuations:
+                analyzed = analyzed + char
+        params = {"purpose": "Remove Puncs", "analyze_text": analyzed}
+        return render(request, "analyze.html", params)
+    else:
+        return HttpResponse("Error")
+
